e96e0de187de5f93b8eb095c373e3335b274d29c
yura jung <jungyr24@hanmail.net>
2021-09-14 21:42:50 +0900

    카카오 2021 상반기

--- 카카오/2021 상반기/신규아이디추천.py
+++ 카카오/2021 상반기/신규아이디추천.py
@@ -23,7 +23,7 @@ def remove_forbidden_str(id):
     return ''.join(tmp)
 def check_count_period(id):
     id = list(id)
-    
+
     tmp = []
     is_period = False
     for s in id:
@@ -71,25 +71,63 @@ def under_len_two(id):
     return ''.join(id)
 def solution(new_id):
     answer = upper_to_lower(new_id)
-    print(answer)
     answer = remove_forbidden_str(answer)
-    print(answer)
     answer = check_count_period(answer)
-    print(answer)
     answer = remove_period(answer)
-    print(answer)
     answer = check_length(answer)
-    print(answer)
     answer = is_empty(answer)
-    print(answer)
     answer = under_len_two(answer)
     
     return answer
 
 
-# print(solution("...!@BaT#*..y.abcdefghijklm")) # bat.y.abcdefghi
-# print(solution("z-+.^."))
+print(solution("...!@BaT#*..y.abcdefghijklm")) # bat.y.abcdefghi
+print(solution("z-+.^."))
 print(solution("=.="))
-# print(solution("123_.def"))
-# print(solution("abcdefghijklmn.p")) #abcdefghijklmn
-# print(solution("......a......a......a....."))
+print(solution("123_.def"))
+print(solution("abcdefghijklmn.p")) #abcdefghijklmn
+print(solution("......a......a......a....."))
+
+# ----------------------------------------------------------------------------
+def solution2(new_id):
+    answer = ''
+    # 1
+    new_id = new_id.lower()
+    # 2
+    for c in new_id:
+        if c.isalpha() or c.isdigit() or c in ['-', '_', '.']:
+            answer += c
+    # 3
+    while '..' in answer:
+        answer = answer.replace('..', '.')
+    # 4
+    if answer[0] == '.':
+        answer = answer[1:] if len(answer) > 1 else '.'
+    if answer[-1] == '.':
+        answer = answer[:-1]
+    # 5
+    if answer == '':
+        answer = 'a'
+    # 6
+    if len(answer) > 15:
+        answer = answer[:15]
+        if answer[-1] == '.':
+            answer = answer[:-1]
+    # 7
+    while len(answer) < 3:
+        answer += answer[-1]
+    return answer
+
+# ----------------------------------------------------------------------------
+import re
+
+def solution(new_id):
+    st = new_id
+    st = st.lower()
+    st = re.sub('[^a-z0-9\-_.]', '', st)
+    st = re.sub('\.+', '.', st)
+    st = re.sub('^[.]|[.]$', '', st)
+    st = 'a' if len(st) == 0 else st[:15]
+    st = re.sub('^[.]|[.]$', '', st)
+    st = st if len(st) > 2 else st + "".join([st[-1] for i in range(3-len(st))])
+    return st
